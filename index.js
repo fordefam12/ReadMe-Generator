7eeb4a16cac12221850a5f7115dcbb191c6964ab
fordefam12 <136336831+fordefam12@users.noreply.github.com>
2023-07-15 09:11:39 -0500

    list of inquirer

--- index.js
+++ index.js
@@ -3,10 +3,75 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const newFile = require("./output/readMe.md") 
 
-fs.writeFile('readMe.md', 'utf8', (error, data) =>
+console.log('\n================================================\n');
+// code will go here
+inquirer.createPromptModule([{
+    type:"input",
+    name:'title',
+    message:"what is the title of the project?"
+},{
+    type:"input",
+    name:'description',
+    message:"what is the description of your project?"
+},{
+    type:"input",
+    name:'tableOfContents',
+    message:"what is the ttable of content for this project?"
+},{
+    type:"input",
+    name:'installation',
+    message:"Describe the installation process if any:"
+},{
+    type:"input",
+    name:'usage',
+    message:"what is this project used for ?"
+},{
+    type:"list",
+    name:'License',
+    message:"what licenses were used for the project?",
+    choices: [
+        "Apache",
+        "Academic",
+        "GNU",
+        "ISC",
+        "MIT",
+        "Mozilla",
+        "Open"
+    ]
+},{
+    type:"input",
+    name:'Contributing',
+    message:"who all contributed on this project?"
+},{
+    type:"input",
+    name:'title',
+    message:"what is the title of the project?"
+},{
+    type:"input",
+    name:'title',
+    message:"what is the title of the project?"
+},])
+console.log('\n================================================\n');
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
+
+fs.writeFile('readMe.md', data, (error, data) =>
   error ? console.error(error) : console.log(data)
 );
-// path needs to be the ouput folder where the new reade me goes.
+// path needs to be the ouput folder where the new read me goes.
 
 // license and badges will be tricky
 
